e29f9d00bbbf6488f11ea3f63dcce6c090b927d7
HARSHA VARDHAN V <v.harshavardhan3@gmail.com>
2022-10-11 23:00:32 +0530

    environment variables added

--- tesseract/settings.py
+++ tesseract/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vzp-!0sfj7dd-h7(x1d16n!(#0ycb$8_l5+^nvdzarqtozysd$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["SETTINGS_DEBUG"].lower() == "true"
 
 ALLOWED_HOSTS = ['*']
 
