4ba511e81b9998ac8bdd6239c8d987531869d237
HARSHA VARDHAN V <v.harshavardhan3@gmail.com>
2022-10-13 00:11:57 +0530

    user profile frontend and login update

--- cube/urls.py
+++ cube/urls.py
@@ -10,14 +10,12 @@ admin.site.index_title = "Dashboard Tesseract"
 
 
 urlpatterns = [
-    path('', views.index, name='index.html'),
-    path('user/<str:user_name>/dashboard', views.dashboard, name='dashboard'),
-    path('user/<str:user_name>/profile', views.profile, name='profile'),
-    path('login/', views.user_login, name='login.html'),
+    path('', views.index, name='index'),
+    path('login/', views.user_login, name='login'),
     path('signup/', views.user_signup, name='signup'),
     path('logout/', views.user_logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('check_username/', views.check_username, name='checkusername'),
     path('check_email/', views.check_email, name='checkemail'),
-    path('home/', views.home_page, name='home'),
+    path('profile/',views.profile, name='profile'),
 ]
